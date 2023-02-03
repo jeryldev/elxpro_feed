47c1ad7e84cd0f10905539c88b4c9dfc3655c471
jeryldev <jeryldev@gmail.com>
2023-02-03 12:47:05 +0800

    fix feed content and change colors from neutral to gray

--- lib/elxpro_feed_web/page_live/feed/feed.ex
+++ lib/elxpro_feed_web/page_live/feed/feed.ex
@@ -9,23 +9,23 @@ defmodule ElxproFeedWeb.PageLive.Feed do
     ~H"""
     <form
       action="#"
-      class="w-full mt-6 pt-6 border-t border-neutral-600 leading-relaxed flex flex-col group"
+      class="w-full mt-6 pt-6 border-t border-gray-600 leading-relaxed flex flex-col group"
     >
-      <label for="content" class="text-neutral-100">Post your comment</label>
+      <label for="content" class="text-gray-100">Post your comment</label>
       <textarea
         name="content"
         id="content"
         cols="30"
         rows="10"
         placeholder="Post your comment"
-        class="bg-neutral-900 border-0 resize-none h-24 p-4 rounded-sm text-neutral-100 mt-4"
+        class="bg-gray-900 border-0 resize-none h-24 p-4 rounded-sm text-gray-100 mt-4"
       >
       </textarea>
 
       <footer class="max-h-0 invisible group-focus-within:visible group-focus-within:max-h-[none]">
         <button
           type="submit"
-          class="px-4 py-6 mt-4 rounded-lg bg-blue-900 not:disabled:hover:bg-blue-600 text-white font-bold cursor-pointer transition disabled:opacity-70 disabled:cursor-not-allowed"
+          class="px-4 py-6 mt-4 rounded-lg bg-blue-900 [&:not(:disabled)]:hover:bg-blue-700 text-white font-bold cursor-pointer transition disabled:opacity-70 disabled:cursor-not-allowed"
         >
           Make a Comment
         </button>
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -2,52 +2,33 @@ alias ElxproFeed.Comments
 alias ElxproFeed.Feeds.Feed
 alias ElxproFeed.Repo
 
+content = "hi
+==
+
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius facilis doloremque fugit.!!!
+
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius facilis doloremque fugit.!!!
+
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius facilis doloremque fugit.!!!
+
+  [elxpro/gustavo](https://www.linkedin.com/in/gustavo-oliveira-642b23aa/)"
+
 feed_1 = %{
-  content: "
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis aliquid culpa soluta.
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi maiores culpa?
-
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus voluptas ipsam.
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo dicta odio.
-
-    [elxpro/jeryl](https://www.linkedin.com/in/jeryldev/)
-  ",
+  content: content,
   author: "Jeryl Estopace",
   avatar_url: "https://avatars.githubusercontent.com/u/26645913?v=4",
   role: "Software Engineer"
 }
 
 feed_2 = %{
-  content: "
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum laboriosam laborum quam?
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore vel animi tenetur.
-
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti maxime explicabo exercitationem.
-
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quia libero velit.
-
-    [elxpro/gustavo](https://www.linkedin.com/in/gustavo-oliveira-642b23aa/)
-  ",
+  content: content,
   author: "Gustavo Oliviera",
   avatar_url: "https://avatars.githubusercontent.com/u/26645913?v=4",
   role: "Elixir Developer"
 }
 
 feed_3 = %{
-  content: "
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam eligendi non temporibus!
-
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis! Ad, harum!
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, perspiciatis nisi. Rem!
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae est odit labore!
-
-    [elxpro/joe](https://www.linkedin.com/in/joedmorris/)
-  ",
+  content: content,
   author: "Joe Morris",
   avatar_url: "https://avatars.githubusercontent.com/u/26645913?v=4",
   role: "Principal Engineer"
