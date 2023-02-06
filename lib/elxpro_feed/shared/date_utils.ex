4e1ec4d5a1b62cb0c2abf5219803724f6289107d
jeryldev <jeryldev@gmail.com>
2023-02-06 16:01:25 +0800

    add shared date_utils

--- lib/elxpro_feed/shared/date_utils.ex
+++ lib/elxpro_feed/shared/date_utils.ex
@@ -1,11 +1,19 @@
 defmodule ElxproFeed.Shared.DateUtils do
+  def format_date(date) do
+    Timex.format!(date, "%Y-%m-%d %H:%M:%S %p", :strftime)
+  end
+
   def published_at(date) do
-    cond do
-      Timex.diff(DateTime.utc_now(), date, :hour) <= 24 -> convert_to_number(date, :hours)
-      Timex.diff(DateTime.utc_now(), date, :day) <= 30 -> convert_to_number(date, :days)
-      Timex.diff(DateTime.utc_now(), date, :month) <= 12 -> convert_to_number(date, :months)
-      true -> convert_to_number(date, :years)
-    end
+    published_at =
+      cond do
+        Timex.diff(DateTime.utc_now(), date, :minute) <= 60 -> convert_to_number(date, :minutes)
+        Timex.diff(DateTime.utc_now(), date, :hour) <= 24 -> convert_to_number(date, :hours)
+        Timex.diff(DateTime.utc_now(), date, :day) <= 30 -> convert_to_number(date, :days)
+        Timex.diff(DateTime.utc_now(), date, :month) <= 12 -> convert_to_number(date, :months)
+        true -> convert_to_number(date, :years)
+      end
+
+    published_at <> " ago"
   end
 
   defp convert_to_number(date, type) do
